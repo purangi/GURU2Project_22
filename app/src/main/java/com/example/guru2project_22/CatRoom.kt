262f1feb2f05deffad04d4a67a1c02a70cd1151f
sohee7528 <sohee7528@hanmail.net>
2022-01-25 15:50:28 +0900

    test

--- app/src/main/java/com/example/guru2project_22/CatRoom.kt
+++ app/src/main/java/com/example/guru2project_22/CatRoom.kt
@@ -7,5 +7,6 @@ class CatRoom : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cat_room)
+
     }
 }
