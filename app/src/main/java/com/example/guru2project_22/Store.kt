b8706f5483f3166083355c771c6d48ff7f6f7b40
sohee7528 <sohee7528@hanmail.net>
2022-01-27 01:56:42 +0900

    made catroom xml, add intent

--- app/src/main/java/com/example/guru2project_22/Store.kt
+++ app/src/main/java/com/example/guru2project_22/Store.kt
@@ -1,17 +1,46 @@
 package com.example.guru2project_22
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.Toast
 
 class Store : AppCompatActivity() {
+
+    lateinit var backbtn : ImageButton
+    lateinit var okbtn : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_store)
 
+        backbtn = findViewById(R.id.backbtn)
+        okbtn=findViewById(R.id.okbtn)
 
 
+
+        backbtn.setOnClickListener {
+            var intent = Intent(this, CatRoom::class.java)
+            startActivity(intent)
+        }
+
+        okbtn.setOnClickListener {
+            var intent = Intent(this, CatRoom::class.java)
+            startActivity(intent)
+
+            val mycoin=200
+            val price =100
+
+            if(mycoin>=price){
+                Toast.makeText(this, "구매했다 냥",Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this, "코인이 부족하다 냥",Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 }
