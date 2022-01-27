aa7f5ea3b7a13eb2572547e14767be7a52d697c5
joohyun2 <ysm1459@naver.com>
2022-01-27 15:45:19 +0900

    activity_main

--- app/src/main/java/com/example/guru2project_22/MainActivity.kt
+++ app/src/main/java/com/example/guru2project_22/MainActivity.kt
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private val DayActivity = DayActivity()
+    private val MainActivity = MainActivity()
+    private val CatRoom = CatRoom()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
