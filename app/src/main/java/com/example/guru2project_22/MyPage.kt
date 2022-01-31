bff82bec9341877d98e36cf500b46882eca65bdf
sojin <sojin49@naver.com>
2022-01-31 17:25:34 +0900

    오류나는 부분 수정

--- app/src/main/java/com/example/guru2project_22/MyPage.kt
+++ app/src/main/java/com/example/guru2project_22/MyPage.kt
@@ -2,9 +2,12 @@ package com.example.guru2project_22
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import com.example.guru2project_22.databinding.ActivityMainBinding
@@ -12,21 +15,27 @@ import com.example.guru2project_22.databinding.ActivityMainBinding
 
 class MyPage : AppCompatActivity() {
     lateinit var myNickname: TextView
-    lateinit var pencilButton: Button
+    lateinit var pencilButton: ImageButton
+    lateinit var catView: View
     private lateinit var binding: ActivityMainBinding
-    var sharedPreference = getSharedPreferences("user", 0)
+    private lateinit var binding2: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_page)
 
+        var pref = getSharedPreferences("user", Context.MODE_PRIVATE)
+
         myNickname = findViewById(R.id.myNickname)
         pencilButton = findViewById(R.id.pencilButton)
-        myNickname.setText(sharedPreference.getString("nickname", ""))
+        catView = findViewById(R.id.catView)
+        myNickname.text = pref.getString("nickname", "").toString()
 
         //뷰 바인딩
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding2 = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding2.root)
 
         //연필 버튼 누를 시
         pencilButton.setOnClickListener {
@@ -42,11 +51,12 @@ class MyPage : AppCompatActivity() {
 
             //변경 버튼 누를 시
             changeButton.setOnClickListener {
-                myNickname.setText(editNickname.text.toString())
+                myNickname.text = editNickname.text.toString()
                 mAlertDialog.dismiss()
             }
         }
 
+        catView = LayoutInflater.from(this).inflate(R.layout.activity_cat_room, null)
 
 
     }
