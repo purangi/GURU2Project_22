6893fa4db433ac77f10075614e88b36bdb0ea726
joohyun2 <ysm1459@naver.com>
2022-02-03 12:32:11 +0900

    activity_main

--- app/src/main/java/com/example/guru2project_22/MainActivity.kt
+++ app/src/main/java/com/example/guru2project_22/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.example.guru2project_22
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import com.google.android.material.bottomnavigation.BottomNavigationMenu
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    private val MyPage = MyPage()
     private val DayActivity = DayActivity()
     private val MainActivity = MainActivity()
-    private val CatRoom = CatRoom()
+    lateinit var catbutton : ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        catbutton.setOnClickListener{
+            val intent=Intent(this, CatRoom::class.java)
+            startActivity(intent)
+        }
     }
 }
