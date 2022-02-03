0eeb798235d3d48d361300ae3bedfb61b50ff171
purangi <kaya0728@naver.com>
2022-02-03 16:48:51 +0900

    완료 버튼 만들기 commit

--- app/src/main/java/com/example/guru2project_22/HabitActivity.kt
+++ app/src/main/java/com/example/guru2project_22/HabitActivity.kt
@@ -1,7 +1,6 @@
 package com.example.guru2project_22
 
 import android.app.Activity
-import android.app.AppComponentFactory
 import android.app.TimePickerDialog
 import android.content.Context
 import android.content.Intent
@@ -13,8 +12,6 @@ import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import java.text.DecimalFormat
 import java.text.SimpleDateFormat
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 import java.util.*
 
 
@@ -24,9 +21,11 @@ class HabitActivity : AppCompatActivity() {
     lateinit var editHabit : EditText
     lateinit var btnStart : Button
     lateinit var btnEnd : Button
-    lateinit var cbOneMonth : CheckBox
-    lateinit var cbTwoMonth : CheckBox
-    lateinit var cbThreeMonth : CheckBox
+    lateinit var rgRepeat : RadioGroup
+    lateinit var rbOneMonth : RadioButton
+    lateinit var rbTwoMonth : RadioButton
+    lateinit var rbThreeMonth : RadioButton
+    lateinit var rbDays : RadioButton
     lateinit var btnSun : Button
     lateinit var btnMon : Button
     lateinit var btnTues : Button
@@ -34,6 +33,7 @@ class HabitActivity : AppCompatActivity() {
     lateinit var btnThur : Button
     lateinit var btnFri : Button
     lateinit var btnSat : Button
+    lateinit var rgAlarm : RadioGroup
     lateinit var rbOn : RadioButton
     lateinit var rbOff : RadioButton
     lateinit var btnComplete : Button
@@ -54,9 +54,11 @@ class HabitActivity : AppCompatActivity() {
         editHabit = findViewById(R.id.editHabit)
         btnStart = findViewById(R.id.btnStart)
         btnEnd = findViewById(R.id.btnEnd)
-        cbOneMonth = findViewById(R.id.cbOneMonth)
-        cbTwoMonth = findViewById(R.id.cbTwoMonth)
-        cbThreeMonth = findViewById(R.id.cbThreeMonth)
+        rgRepeat = findViewById(R.id.rgRepeat)
+        rbOneMonth = findViewById(R.id.rbOneMonth)
+        rbTwoMonth = findViewById(R.id.rbTwoMonth)
+        rbThreeMonth = findViewById(R.id.rbThreeMonth)
+        rbDays = findViewById(R.id.rbDays)
         btnSun = findViewById(R.id.btnSun)
         btnMon = findViewById(R.id.btnMon)
         btnTues = findViewById(R.id.btnTues)
@@ -64,11 +66,14 @@ class HabitActivity : AppCompatActivity() {
         btnThur = findViewById(R.id.btnThur)
         btnFri = findViewById(R.id.btnFri)
         btnSat = findViewById(R.id.btnSat)
+        rgAlarm = findViewById(R.id.rgAlarm)
         rbOn = findViewById(R.id.rbOn)
         rbOff = findViewById(R.id.rbOff)
         btnComplete = findViewById(R.id.btnComplete)
 
-        // 현재시간을 가져오기
+        rbOff.isChecked = true
+
+        // 현재 시간을 가져오기
         val now = System.currentTimeMillis()
         val date = Date(now)
         val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale("ko", "KR"))
@@ -119,18 +124,80 @@ class HabitActivity : AppCompatActivity() {
             ).show()
         }
 
+        var days = ""//각 요일이 선택되었으면 0, 아니면 1
+
+        //days 설정
+        when(btnSun.isSelected) {
+            true -> days += "1"
+            false -> days += "0"
+        }
+        when(btnMon.isSelected) {
+            true -> days += "1"
+            false -> days += "0"
+        }
+        when(btnTues.isSelected) {
+            true -> days += "1"
+            false -> days += "0"
+        }
+        when(btnWed.isSelected) {
+            true -> days += "1"
+            false -> days += "0"
+        }
+        when(btnThur.isSelected) {
+            true -> days += "1"
+            false -> days += "0"
+        }
+        when(btnFri.isSelected) {
+            true -> days += "1"
+            false -> days += "0"
+        }
+        when(btnSat.isSelected) {
+            true -> days += "1"
+            false -> days += "0"
+        }
+
+        var isDaysSelected = btnSun.isSelected || btnMon.isSelected || btnTues.isSelected || btnWed.isSelected || btnThur.isSelected || btnFri.isSelected || btnSat.isSelected
+
+        var alarm : Int //꺼져있으면 0, 켜져있으면 1
+
+        when(rbOff.isChecked) { //알람 Int 설정
+            true -> alarm = 0
+            false -> alarm = 1
+        }
+
         btnComplete.setOnClickListener {
             //필수 선택지들이 입력되지 않았을 시 완료 선택 불가
             //scheduleDB (date text, icon text, habit text, startTime INTEGER, endTime INTEGER, days INTEGER, alarm INTEGER)
             var dateText = str_date
-            var iconText = iconName
+            //var iconText = iconName //안쓰고 직접 넣어도..?
             var habitText = editHabit.text.toString()
             var startTime = btnStart.text
             var endTime = btnEnd.text
-            var days : Int = 1011011 //첫자리가 0인거 지원 x 아마 text로 전향해야할지도
-            var alarm = 0 //꺼져있으면 0, 켜져있으면 1
+            //days, alarm 입력
+
+            //입력값 확인
+            if(dateText.isEmpty()) {
+                Toast.makeText(this@HabitActivity, "잘못된 접근입니다", Toast.LENGTH_SHORT).show() //나중에는 그냥 오늘 시간 할당으로
+            } else if(iconName.isEmpty()) {
+                Toast.makeText(this@HabitActivity, "아이콘을 선택해주세요", Toast.LENGTH_SHORT).show()
+            } else if(habitText.isEmpty()) {
+                Toast.makeText(this@HabitActivity, "가지고 싶은 습관을 입력해주세요", Toast.LENGTH_SHORT).show()
+            } else if(startTime==endTime) {
+                Toast.makeText(this@HabitActivity, "시작 시간과 끝나는 시간이 같습니다", Toast.LENGTH_SHORT).show()
+            } else if(rgRepeat.isSelected && !isDaysSelected) {
+                Toast.makeText(this@HabitActivity, "반복할 요일을 선택해주세요", Toast.LENGTH_SHORT).show()
+            } else if(!rgRepeat.isSelected && isDaysSelected) {
+                Toast.makeText(this@HabitActivity, "반복할 기간을 선택해주세요", Toast.LENGTH_SHORT).show()
+            } else if(rgAlarm.isSelected == null) {
+                Toast.makeText(this@HabitActivity, "알람 설정 여부를 확인해주세요", Toast.LENGTH_SHORT).show()
+            } else { //전부 입력되었으면 데이터 베이스 입력 & day로 인텐트
+
+                //반복 설정에 따라 db 추가
 
 
+                //입력 후 인텐트 이동 및 토스트
+                Toast.makeText(this, "습관을 입력했습니다.", Toast.LENGTH_SHORT).show()
+            }
         }
 
 
@@ -166,7 +233,7 @@ class HabitActivity : AppCompatActivity() {
 
     fun clickBtn(view: View) {
         var button = view as Button
-        button?.isSelected = button?.isSelected != true
+        button.isSelected = button.isSelected != true
 
     }
 
@@ -177,7 +244,7 @@ class HabitActivity : AppCompatActivity() {
         }
 
         override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-            TODO("Not yet implemented")
+            //아직 필요성x
         }
     }
 }
