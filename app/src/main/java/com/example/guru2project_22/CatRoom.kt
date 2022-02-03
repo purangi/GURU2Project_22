2bfe4cba452cd5217cf18b24b91cd7af338aa1f9
sohee7528 <sohee7528@hanmail.net>
2022-02-03 16:10:50 +0900

    -

--- app/src/main/java/com/example/guru2project_22/CatRoom.kt
+++ app/src/main/java/com/example/guru2project_22/CatRoom.kt
@@ -1,11 +1,14 @@
 package com.example.guru2project_22
 
 import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
+import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.drawerlayout.widget.DrawerLayout
 import org.w3c.dom.Text
@@ -14,9 +17,18 @@ class CatRoom : AppCompatActivity() {
 
     lateinit var backbtn : ImageButton
     lateinit var storebtn : ImageButton
-    lateinit var item : ImageButton
-    //lateinit var drawer : DrawerLayout
     lateinit var mycoin : TextView
+    lateinit var origincat : ImageView
+    lateinit var catwindow : ImageView
+    lateinit var catfood : ImageView
+    lateinit var catbookshelf : ImageView
+    lateinit var catsmallplant : ImageView
+    lateinit var catbigplant : ImageView
+    lateinit var catbathroom : ImageView
+    lateinit var catbed : ImageView
+    lateinit var catcircle : ImageView
+    lateinit var cathome : ImageView
+    lateinit var catsofa : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,11 +36,27 @@ class CatRoom : AppCompatActivity() {
 
         backbtn = findViewById(R.id.backbtn)
         storebtn = findViewById(R.id.storebtn)
-        item = findViewById(R.id.item)
-        //drawer = findViewById(R.id.drawer)
         mycoin = findViewById(R.id.mycoin)
+        origincat = findViewById(R.id.originCat)
+        catwindow = findViewById(R.id.cat_window)
+        catfood = findViewById(R.id.cat_catfood)
+        catbookshelf = findViewById(R.id.cat_bookshelf)
+        catsmallplant = findViewById(R.id.cat_smallplant)
+        catbigplant = findViewById(R.id.cat_bigplant)
+        catbathroom = findViewById(R.id.cat_catbathroom)
+        catbed = findViewById(R.id.cat_catbed)
+        catcircle =findViewById(R.id.cat_circle2)
+        cathome =findViewById(R.id.cat_home)
+        catsofa =findViewById(R.id.cat_sofa)
 
         loadCoin()
+
+        var type = intent.getStringExtra("number")
+        when(type){
+            "1" -> catfood.visibility = View.VISIBLE
+        }
+
+
 
         backbtn.setOnClickListener {
             var intent = Intent(this, MainActivity::class.java)
@@ -39,21 +67,12 @@ class CatRoom : AppCompatActivity() {
             startActivity(intent)
         }
 
-        item.setOnClickListener {
-            /*if(!drawer.isDrawerOpen(Gravity.RIGHT)){
-                drawer.openDrawer(Gravity.RIGHT)
-            }*/
-
-        }
 
     }
 
-    private fun loadCoin() {
+   private fun loadCoin() {
         var pref = this.getPreferences(0)
-        var coin = pref.getInt("MYCOIN",0)
-
-        if(coin != 0){
-            mycoin.setText(coin.toString())
-        }
+        var coin = pref.getInt("mycoin",0)
+        mycoin.text=coin.toString()
     }
 }
