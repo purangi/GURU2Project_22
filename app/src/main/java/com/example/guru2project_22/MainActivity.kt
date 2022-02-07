ea8aa4ab035934753cd3c2dd56a859c3e03763be
purangi <kaya0728@naver.com>
2022-02-07 14:17:27 +0900

    main db 방식으로 수정 commit

--- app/src/main/java/com/example/guru2project_22/MainActivity.kt
+++ app/src/main/java/com/example/guru2project_22/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.guru2project_22
 
 import android.content.Intent
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +24,8 @@ class MainActivity : AppCompatActivity() {
     var mycoin = 0
     var iconName = ""
 
+    lateinit var dbManager: HabitActivity.DBManager
+    lateinit var sqlitedb: SQLiteDatabase
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,12 +34,11 @@ class MainActivity : AppCompatActivity() {
 
         val listview: ListView
         val adapter: CustomChoiceListViewAdapter
-        var iconimg = intent.getIntExtra("icon", 0)
 
-        //intent값 받아오기
-        var Intent = getIntent()
-        var editHabit: EditText = findViewById(R.id.editHabit)
-        textview3.setText(intent.getIntExtra("text", 0).toString())
+//        //intent값 받아오기
+//        var Intent = getIntent()
+//        var editHabit: EditText = findViewById(R.id.editHabit)
+//        textview3.setText(intent.getIntExtra("text", 0).toString())
 
         //코인
         val pref = getSharedPreferences("user", 0)
@@ -48,27 +53,40 @@ class MainActivity : AppCompatActivity() {
         }
 
         //네비게이션 이미지 버튼
-        navday.setOnClickListener{
+        navday.setOnClickListener {
             val intent = Intent(this, DayActivity::class.java)
             startActivity(intent)
         }
 
-        navhome.setOnClickListener{
+        navhome.setOnClickListener {
             val intent = Intent(this, com.example.guru2project_22.MainActivity::class.java)
             startActivity(intent)
         }
 
-        navmypage.setOnClickListener{
+        navmypage.setOnClickListener {
             val intent = Intent(this, MyPage::class.java)
             startActivity(intent)
         }
 
+        //오늘 날짜
+        var cal: Calendar = Calendar.getInstance()
+        var date = cal.time
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale("ko", "KR"))
+        var dateText = dateFormat.format(date)
+
+        //dbManager
+        dbManager = HabitActivity.DBManager(this, "scheduleDB", null, 1)
+        sqlitedb = dbManager.readableDatabase
+        var cursor: Cursor
+
+        cursor = sqlitedb.rawQuery(
+            "SELECT * FROM scheduleDB WHERE date = '" + dateText + "' ORDER BY startTime",
+            null
+        )
 
         //캘린더 날짜 선택 시
         //일정 표시
         calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
-            iconName = iconimg.toString()
-            textview3.text = editHabit.text
 
             //DayActivity로 이동
             val intent = Intent(this, DayActivity::class.java)
@@ -83,52 +101,77 @@ class MainActivity : AppCompatActivity() {
         listview = findViewById<View>(R.id.listview) as ListView
         listview.adapter = adapter
 
-        // 첫 번째 아이템 추가.
-        adapter.addItem(
-            ContextCompat.getDrawable(this, iconimg),
-            editHabit.toString()
-        )
-        // 두 번째 아이템 추가.
-        adapter.addItem(
-            ContextCompat.getDrawable(this, iconimg),
-            editHabit.toString()
-        )
-        // 세 번째 아이템 추가.
-        adapter.addItem(
-            ContextCompat.getDrawable(this, iconimg),
-            editHabit.toString()
-        )
-        // 네 번째 아이템 추가.
-        adapter.addItem(
-            ContextCompat.getDrawable(this, iconimg),
-            editHabit.toString()
-        )
-        // 다섯 번째 아이템 추가.
-        adapter.addItem(
-            ContextCompat.getDrawable(this, iconimg),
-            editHabit.toString()
-        )
-    }
+        while (cursor.moveToNext()) {
+            var habit = cursor.getString(cursor.getColumnIndex("habit")).toString()
+            var icon = cursor.getString(cursor.getColumnIndex("icon")).toString()
 
-    //체크박스 체크 시
-
-    fun onCheckboxClicked(view: View) {
-        if (view is CheckBox) {
-            val checked: Boolean = view.isChecked
-
-            when (view.id) {
-                R.id.checkBox -> {
-                //토스트 메시지 출력
-                Toast.makeText(this@MainActivity, "10코인 적립!", Toast.LENGTH_LONG).show()
-                    checkbox.setChecked(false)
-                //코인 받아와서 +10한 후 반환
-                val pref = getSharedPreferences("user", 0)
-                val editor = pref.edit()
-                mycoin = (pref.getString("coin", "100"))!!.toInt()
-                mycoin += 10
-                editor.putString("coin", mycoin.toString())
-                editor.apply()
+            var iconimg = R.drawable.catjelly
+            when (icon) {
+                "water" -> iconimg = R.drawable.water
+                "bed" -> iconimg = R.drawable.bed
+                "computer" -> iconimg = R.drawable.computer
+                "exercise" -> iconimg = R.drawable.exercise
+                "sleep" -> iconimg = R.drawable.moon
+                "study" -> iconimg = R.drawable.study
+                "reading" -> iconimg = R.drawable.reading
+                "pill" -> iconimg = R.drawable.pill
+                "meal" -> iconimg = R.drawable.meal
+                "shopping" -> iconimg = R.drawable.shopping
+                "bath" -> iconimg = R.drawable.bath
+                "cleanup" -> iconimg = R.drawable.cleanup
             }
+            adapter.addItem(
+                ContextCompat.getDrawable(this, iconimg),
+                habit.toString()
+            )
+        }
+//        // 첫 번째 아이템 추가.
+//        adapter.addItem(
+//            ContextCompat.getDrawable(this, iconimg),
+//            editHabit.toString()
+//        )
+//        // 두 번째 아이템 추가.
+//        adapter.addItem(
+//            ContextCompat.getDrawable(this, iconimg),
+//            editHabit.toString()
+//        )
+//        // 세 번째 아이템 추가.
+//        adapter.addItem(
+//            ContextCompat.getDrawable(this, iconimg),
+//            editHabit.toString()
+//        )
+//        // 네 번째 아이템 추가.
+//        adapter.addItem(
+//            ContextCompat.getDrawable(this, iconimg),
+//            editHabit.toString()
+//        )
+//        // 다섯 번째 아이템 추가.
+//        adapter.addItem(
+//            ContextCompat.getDrawable(this, iconimg),
+//            editHabit.toString()
+//        )
+//    }
+
+        //체크박스 체크 시
+
+        fun onCheckboxClicked(view: View) {
+            if (view is CheckBox) {
+                val checked: Boolean = view.isChecked
+
+                when (view.id) {
+                    R.id.checkBox -> {
+                        //토스트 메시지 출력
+                        Toast.makeText(this@MainActivity, "10코인 적립!", Toast.LENGTH_LONG).show()
+                        checkbox.setChecked(false)
+                        //코인 받아와서 +10한 후 반환
+                        val pref = getSharedPreferences("user", 0)
+                        val editor = pref.edit()
+                        mycoin = (pref.getString("coin", "100"))!!.toInt()
+                        mycoin += 10
+                        editor.putString("coin", mycoin.toString())
+                        editor.apply()
+                    }
+                }
             }
         }
     }
