3acfab17c8df831b96a3c8e09e747c4893754d02
joohyun2 <ysm1459@naver.com>
2022-02-07 14:57:30 +0900

    activity_main

--- app/src/main/java/com/example/guru2project_22/CustomChoiceListViewAdapter.kt
+++ app/src/main/java/com/example/guru2project_22/CustomChoiceListViewAdapter.kt
@@ -3,12 +3,11 @@ package com.example.guru2project_22
 import android.R
 import android.content.Context
 import android.graphics.drawable.Drawable
-import android.os.Parcel
-import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.CheckBox
 import android.widget.ImageView
 import android.widget.TextView
 
@@ -39,6 +38,7 @@ class CustomChoiceListViewAdapter  // ListViewAdapter의 생성자
         // 화면에 표시될 View(Layout이 inflate된)으로부터 위젯에 대한 참조 획득
         val iconImageView: ImageView = convertView?.findViewById(R.id.icon) as ImageView
         val textTextView = convertView.findViewById(R.id.text1) as TextView
+        val checkCheckBoxBox = convertView.findViewById(R.id.checkbox) as CheckBox
 
         // Data Set(listViewItemList)에서 position에 위치한 데이터 참조 획득
         val listViewItem = listViewItemList[position]
@@ -60,7 +60,7 @@ class CustomChoiceListViewAdapter  // ListViewAdapter의 생성자
     }
 
     // 아이템 데이터 추가를 위한 함수. 개발자가 원하는대로 작성 가능.
-    fun addItem(icon1: Drawable?, text1: String?) {
+    fun addItem(icon1: Drawable?, text1: String?, checkbox: CheckBox) {
         val item = ListViewItem()
         item.setIcon(icon1)
         item.setText(text1)
