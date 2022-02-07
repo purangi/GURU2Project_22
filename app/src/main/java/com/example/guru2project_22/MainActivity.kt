2c264f58c7d22253573f0d6fc971e4105a3d95f7
joohyun2 <ysm1459@naver.com>
2022-02-07 13:15:28 +0900

    activity_main

--- app/src/main/java/com/example/guru2project_22/MainActivity.kt
+++ app/src/main/java/com/example/guru2project_22/MainActivity.kt
@@ -6,11 +6,13 @@ import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.example.guru2project_22.databinding.ActivityMyPageBinding
 
 
 class MainActivity : AppCompatActivity() {
     private val DayActivity = DayActivity()
     private val MainActivity = MainActivity()
+    private lateinit var binding: ActivityMyPageBinding
     lateinit var catbutton: ImageButton
     lateinit var navday: ImageButton
     lateinit var navhome: ImageButton
@@ -26,6 +28,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val listview: ListView
         val adapter: CustomChoiceListViewAdapter
         var iconimg = intent.getIntExtra("icon", 0)
@@ -48,17 +51,17 @@ class MainActivity : AppCompatActivity() {
         }
 
         //네비게이션 이미지 버튼
-        navday.setOnClickListener {
+        binding.navday.setOnClickListener{
             val intent = Intent(this, DayActivity::class.java)
             startActivity(intent)
         }
 
-        navhome.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
+        binding.navhome.setOnClickListener{
+            val intent = Intent(this, com.example.guru2project_22.MainActivity::class.java)
             startActivity(intent)
         }
 
-        navmypage.setOnClickListener {
+        binding.navmypage.setOnClickListener{
             val intent = Intent(this, MyPage::class.java)
             startActivity(intent)
         }
